d84d64e72a17c4c3cb9769bf629f6576b1038e4e
MaameAndoh <115769701+MaameAndoh@users.noreply.github.com>
2024-01-09 20:22:49 -0500

    Add files via upload

---
+++ Course.java
@@ -0,0 +1,9 @@
+public class Course {
+    protected String courseName = "";
+    Course(String c) {
+        this.courseName = c;
+    }
+
+    public String getCourse(){return this.courseName;}
+
+}
